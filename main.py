f30bd6c322d272cd51c94466f77f69a45b652a80
bpedrero <bspedrero@gmail.com>
2023-12-05 19:37:46 -0300

    segundo commit

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-""""
+"""
 Connecting to the ACI Server:
 This ACI Sandbox Lab contains a shared ACI Server Instance.
-The server can be connected using login credentials admin and password : !v3G@!4@Y and is located at
+The server can be connected using login credentials
 user: admin
-pass: !v3G@!4@Y
-url: https://sandboxapicdc.cisco.com
+password: !v3G@!4@Y
+url = https://sandboxapicdc.cisco.com
 """
 """
 POST http://APIC-IP/api/aaaLogin.json HTTP/1.1
@@ -21,21 +21,21 @@ Content-Type: application/json
 """
 import requests
 import json
+import conf
 
 requests.packages.urllib3.disable_warnings()
-url="https://sandboxapicdc.cisco.com/api/aaaLogin.json"
-dato= {
+url = "https://sandboxapicdc.cisco.com/api/aaaLogin.json"
+dato = {
     "aaaUser" : {
         "attributes" : {
-            "name" : "admin",
-            "pwd" : "!v3G@!4@Y"
+            "name" : "conf.usuario",
+            "pwd" : "conf.clave"
         }
     }
 }
 cabecera = {"Content-Type": "application/json"}
-respuesta = requests.post(url,data=json.dumps(dato),headers=cabecera,verify=False)
+
+respuesta = requests.post(url, data=json.dumps(dato), headers=cabecera,verify=False)
 respuesta_json = respuesta.json()
 print(respuesta_json)
-Token = respuesta_json["imdata"][0]["aaaLogin"]["attributes"]["token"]
 
-print ("API-Token: ",Token)
